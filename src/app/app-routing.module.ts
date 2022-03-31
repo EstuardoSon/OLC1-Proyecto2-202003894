829b04fe8d3d2667402685125cecf83a21cb930f
Estuardo Son <2986622050101@ingenieria.usac.edu.gt>
2022-03-31 00:31:26 -0600

    segundo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo:'Editor',
+    pathMatch:'full'
+  },
+  {
+    path:'Editor',
+    component: EditorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
